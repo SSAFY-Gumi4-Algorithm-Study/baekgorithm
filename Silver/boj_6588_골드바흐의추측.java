4afe75753d284a52b6dd8d4e4c98fec1b705d048
Hyeonsu KIM <wshyhs@gmail.com>
2025-10-23 22:07:00 +0900

    boj_6588_골드바흐의추측

---
+++ Silver/boj_6588_골드바흐의추측.java
@@ -0,0 +1,48 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class boj_6588_골드바흐의추측 {
+
+    static boolean[] isPrime;
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        isPrime = new boolean[1000001];
+        for(int i=2; i<=1000000; i++){
+            isPrime[i]=true;
+        }
+        int sqrt = (int)Math.sqrt(1000000);
+        for(int i=2; i<= sqrt; i++){
+            if(isPrime[i]){
+                for(int j= i*i; j<=1000000; j+=i){
+                    
+                    isPrime[j]=false;
+                }
+
+            }
+        
+            
+        }
+        while (true) {
+            int N = Integer.parseInt(br.readLine());
+            if(N == 0 ) break;
+            boolean flag = false;
+            for(int i=2; i<=N/2; i++){
+                
+                if(isPrime[i]&&isPrime[N-i]){
+                    flag=true;
+                    System.out.println(N + " = " + i + " + " + (N - i));
+                    break;
+                }
+            }
+            if(!flag){
+                System.out.println("Goldbach's conjecture is wrong.");
+            }
+
+        }
+        
+    }
+}
