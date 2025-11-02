d4ebe2e766032b9a86a47d4ab7e28e1d98bd5b17
Hyeonsu KIM <wshyhs@gmail.com>
2025-11-02 21:14:37 +0900

    코멘트 받고 수정 compare시간 복잡도 줄임

--- Essential/21608_상어초등학교/Main.java
+++ Essential/21608_상어초등학교/Main.java
@@ -12,7 +12,7 @@ public class Main {
     static int[] dr={-1,1,0,0};
     static int[] dc={0,0,-1,1};
     static Map<Integer,int[]> love; //<학생번호, 학생이 좋아하는 친구들의 번호>
-    static class Seat { //자리에 대한 정보
+    static class Seat  implements Comparable<Seat>{ //자리에 대한 정보
         int r,c, empty, friend;
 
         public Seat(int r, int c, int empty, int friend) {
@@ -20,6 +20,21 @@ public class Main {
             this.c = c;
             this.empty = empty;
             this.friend = friend;
+        }
+
+        @Override
+        public int compareTo(Seat o) {
+            //1.친구가 많은 자리 2.빈칸이 많은자리 3. 행이 작은 자리 4. 열이 작은 자리
+            if(this.friend == o.friend){
+                if(this.empty == o.empty){
+                    if(this.r == o.r){
+                        return this.c - o.c; //친구, 빈 칸, 행이 같다면 열로 오름차순
+                    }
+                    return this.r - o.r; //친구, 빈 칸이 같다면 열로 오름차순
+                }
+                return o.empty - this.empty; //친구가 같다면 빈칸으로 내림차순
+            }
+            return o.friend - this.friend; //친구로 내림차순
         }
 
     }
@@ -51,8 +66,7 @@ public class Main {
 
     static void putStudentSeat(int student){
         int [] friends = love.get(student);
-        ArrayList<Seat> seats = new ArrayList<>(); //모든 자리의 정보를 담을 리스트
-
+        Seat seat = null; // 그 사람의 자리
         for(int r=0; r<N; r++){
             for(int c=0; c<N; c++){ //모든 자리를 돌면서 해당 학생의 앉힐 자리에 대한 정보를 구한다.
                 if(graph[r][c]!=0) continue; //이미 그 자리에 배정 받은 학생이 있다면  skip
@@ -71,27 +85,18 @@ public class Main {
                     if(graph[nr][nc]==0) emptyCnt++; // 빈 칸이라면 빈칸 +1
                 }
 
-                seats.add(new Seat(r, c, emptyCnt, friendCnt)); //해당 정보를 리스트에 넣어준다
-            }
-        }
-
-        seats.sort((o1,o2)->{//1.친구가 많은 자리 2.빈칸이 많은자리 3. 행이 작은 자리 4. 열이 작은 자리
-            if(o1.friend == o2.friend){
-                if(o1.empty == o2.empty){
-                    if(o1.r == o2.r){
-                        return o1.c - o2.c; //친구, 빈 칸, 행이 같다면 열로 오름차순
+                if(seat == null){
+                    seat = new Seat(r, c, emptyCnt, friendCnt);
+                }else{
+                    Seat com = new Seat(r, c, emptyCnt, friendCnt);
+                    if(seat.compareTo(com)>0){
+                        seat = com;
                     }
-                    return o1.r - o2.r; //친구, 빈 칸이 같다면 열로 오름차순
                 }
-                return o2.empty - o1.empty; //친구가 같다면 빈칸으로 내림차순
             }
-            return o2.friend - o1.friend; //친구로 내림차순
-        });
-
-        for(Seat s : seats){
-            graph[s.r][s.c] = student; //가장 처음 seat가 best seat이므로 해당 칸에 학생을 배치하고 return
-            return;
         }
+
+        graph[seat.r][seat.c] =student;
     }
 
     static int getScore(){
