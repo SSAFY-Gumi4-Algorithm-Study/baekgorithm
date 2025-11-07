5c1ad511234fc8f43caeafaa10e0adb047b31719
wshyhs <wshyhs@gmail.com>
2025-11-07 17:13:41 +0900

    브론즈맛있~어

---
+++ bronze/boj_1271.java
@@ -0,0 +1,15 @@
+package baekgorithm.bronze;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class boj_1271 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger N = new BigInteger(sc.next());
+        BigInteger M = new BigInteger(sc.next());
+        System.out.println(N.divide(M));
+        System.out.println(N.remainder(M));
+    }
+}
---
+++ bronze/boj_14581.java
@@ -0,0 +1,14 @@
+package baekgorithm.bronze;
+
+import java.util.Scanner;
+
+public class boj_14581 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String l = sc.next();
+
+        System.out.println(":fan::fan::fan:\n" + //
+                ":fan::" + l + "::fan:\n" + //
+                ":fan::fan::fan:");
+    }
+}
