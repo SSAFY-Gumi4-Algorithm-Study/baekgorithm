29071fbf3db8892786bdc3f99baa493cc58549d8
wshyhs <wshyhs@gmail.com>
2025-11-21 17:58:22 +0900

    브론즈 잘~먹었습니다!

---
+++ bronze/boj_15733.java
@@ -0,0 +1,7 @@
+package baekgorithm.bronze;
+
+public class boj_15733 {
+    public static void main(String[] args) {
+        System.out.println("I'm Sexy");
+    }
+}
---
+++ bronze/boj_27323.java
@@ -0,0 +1,12 @@
+package baekgorithm.bronze;
+
+import java.util.Scanner;
+
+public class boj_27323 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a*b);
+    }
+}
