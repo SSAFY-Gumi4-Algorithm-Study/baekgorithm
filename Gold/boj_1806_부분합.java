9972032f7fcde77fabc47252245c9eca3148735e
wshyhs <wshyhs@gmail.com>
2025-12-26 16:55:54 +0900

    boj_1806_부분합

---
+++ Gold/boj_1806_부분합.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_1806_부분합 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+        int arr[] = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int left = 0;
+
+        int sum = 0;
+        int min_len = Integer.MAX_VALUE;
+        for (int right = 0; right < N; right++) {
+            sum += arr[right];
+
+            while (sum >= S) {
+                min_len = Math.min(min_len, right - left + 1);
+                sum -= arr[left++];
+            }
+        }
+
+        System.out.println(min_len == Integer.MAX_VALUE ? 0 : min_len);
+    }
+}
